35006dfc31c9a73e81fd1c32f56d5a324c180528
Stuart Rivero <stuart.rivero.consultant@springernature.com>
2023-09-12 11:59:50 +0100

    Initial spring-boot app from initialiser

---
+++ src/main/kotlin/com/example/kotlincalculatorapi/KotlinCalculatorApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlincalculatorapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCalculatorApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCalculatorApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlincalculatorapi/KotlinCalculatorApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlincalculatorapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCalculatorApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
